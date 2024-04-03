8c400135938033e6f6df4f74125cdb01020bad96
Logan Dietel <logandietel1@gmail.com>
2024-04-02 23:27:21 -0700

    Finished array methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,75 +1,73 @@
 function forEach(array, cb) {
   for (let i = 0; i < array.length; i++) {
-    cb(array[i], i, array)
+     cb(array[i], i, array);
   }
 }
 
 function map(array, cb) {
-  const newArray = []
-  for (let i = 0; i < array.length; i++) {
-    newArray.push(cb(array[i], i, array))
-  }
-
-  return newArray
+  const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+      newArr.push(cb(array[i], i, array));
+    }
+  return newArr;
 }
 
 function filter(array, cb) {
-  const newArray = []
+  const newArr = [];
   for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-    if (cb(element, i, array)) newArray.push(element)
+    if (cb(array[i], i, array)) newArr.push(array[i]);
   }
-
-  return newArray
+  return newArr;
 }
 
 function reduce(array, cb, initialValue) {
-  let currentValue = initialValue
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-    if (initialValue == null && i === 0) {
-      currentValue = element
-    } else {
-      currentValue = cb(currentValue, element, i, array)
+    let currentValue = initialValue;
+    for (let i = 0; i < array.length; i++) {
+      if (currentValue === undefined && i === 0) {
+        currentValue = array[i];
+      } else {
+        currentValue = cb(currentValue, array[i], i, array);
+      }
     }
-  }
 
-  return currentValue
+    return currentValue;
 }
 
 function some(array, cb) {
-  for (let i = 0; i < array.length; i++) {
-    if (cb(array[i], i, array)) return true
-  }
-
-  return false
+    for (let i = 0; i < array.length; i++) {
+      if (cb(array[i], i, array)) {
+        return true;
+      }
+    }
+    return false;
 }
 
 function every(array, cb) {
   for (let i = 0; i < array.length; i++) {
-    if (!cb(array[i], i, array)) return false
+    if (!cb(array[i], i, array)) {
+      return false;
+    }
   }
-
-  return true
+  return true;
 }
 
 function flat(array, depth = 1) {
-  const newArray = []
+  const newArr = [];
   for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-    if (Array.isArray(element) && depth > 0) {
-      newArray.push(...flat(element, depth - 1))
-    } else {
-      newArray.push(element)
-    }
+      if (Array.isArray(array[i]) && depth > 0) {
+          newArr.push(...flat(array[i], depth - 1));
+      } else {
+        newArr.push(array[i]);
+      }
   }
-  return newArray
+  return newArr;
 }
 
 function find(array, cb) {
   for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-    if (cb(element, i, array)) return element
+    if (cb(array[i], i, array)) {
+      return array[i];
+    }
   }
 }
 
